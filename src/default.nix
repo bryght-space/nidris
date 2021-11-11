7b07d240f4edbb3fa73b53321209f162dc655e79
Marc Esquerra <esquerra@gmail.com>
2021-11-11 16:16:24 +0000

    Add support for inigo

--- src/default.nix
+++ src/default.nix
@@ -1,20 +1,36 @@
 with (import ./helpers.nix);
-let
-  getByRevision = revision: name: builtins.getAttr name (channelFromRevision revision);
-  revisions = {
-    "0.5.1" = ["896ee5ffc634473f63ce70829568c9b8156a13cf" "idris2" ];
-    "0.5.0" = ["948ef8a6c5fd2c71a5e45aecd5cc997740e91024" "idris2" ];
-  };
+version:
+  let
+    getByRevision = revision: name: builtins.getAttr name (channelFromRevision revision);
+    revisions = {
+      "0.5.1" = ["896ee5ffc634473f63ce70829568c9b8156a13cf" "idris2" ];
+      "0.5.0" = ["948ef8a6c5fd2c71a5e45aecd5cc997740e91024" "idris2" ];
+    };
 
-  getByVersion = version :
-    let
-      p        = builtins.getAttr version revisions;
-      revision = builtins.elemAt p 0;
-      items    = builtins.tail p;
-      f        = n: getByRevision revision n;
-    in
-      builtins.map f items;
-      idris = version : getByVersion version;
-      idris2PackagesChannel = idris2PackagesChannelFromRevision 5794a5150e6640352b7e63967a2fc6a56fb12d93;
-      inigo = idris2PackagesChannel.inigo;
-in version : [ (idris version) inigo ]
+    getByVersion = version :
+      let
+        p        = builtins.getAttr version revisions;
+        revision = builtins.elemAt p 0;
+        items    = builtins.tail p;
+        f        = n: getByRevision revision n;
+      in
+        builtins.map f items;
+
+    getStdenvByVersion = version :
+      let
+        p        = builtins.getAttr version revisions;
+        revision = builtins.elemAt p 0;
+        items    = builtins.tail p;
+        f        = n: getByRevision revision n;
+      in
+        builtins.map f ["stdenv"];
+
+    idris = getByVersion version;
+    stdenv = builtins.elemAt (getStdenvByVersion version) 0;
+    idris2PackagesChannel = idris2PackagesChannelFromRevision "5794a5150e6640352b7e63967a2fc6a56fb12d93";
+    inigo = (builtins.getAttr (stdenv.hostPlatform.system) (idris2PackagesChannel.packages)).inigo;
+  in [ idris inigo ]
+
+
+
+
