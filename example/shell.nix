968518d56c5c56815001e2f1faabcf7491a9c012
marcesquerra <esquerra@gmail.com>
2021-11-11 15:02:42 +0000

    [skip actions] Update example

--- example/shell.nix
+++ example/shell.nix
@@ -1,8 +1,8 @@
 { pkgs ? import <nixpkgs> {} }:
 
 let
-  nidris = import (fetchTarball "https://github.com/bryght-space/nidris/archive/6d1a87b32ef5005c0e05bf028fd098bd20bca285.tar.gz");
-  idris = nidris ./.;
+  nidris = import (fetchTarball "https://github.com/bryght-space/nidris/archive/95d65f73c2e6e796191885c9b649edefd6391a42.tar.gz");
+  idris = nidris "0.5.1";
 in
   pkgs.mkShell {
     buildInputs = [
